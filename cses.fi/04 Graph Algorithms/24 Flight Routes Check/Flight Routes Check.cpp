1661753f8749720fdcea33b354e5640648209339
temporary77 <thanabodhi77.n@gmail.com>
2023-11-04 17:24:00 +0800

    push

--- cses.fi/04 Graph Algorithms/24 Flight Routes Check/Flight Routes Check.cpp
+++ cses.fi/04 Graph Algorithms/24 Flight Routes Check/Flight Routes Check.cpp
@@ -1,65 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int disjoint[100001];
-int height[100001];
-int cnt;
-vector<int> vctr;
+vector<int> graph1[100001];
+vector<int> graph2[100001];
+bool visited1[100001];
+bool visited2[100001];
+
+void dfs1(int node) {
+	visited1[node] = 1;
+	for (auto it : graph1[node]) {
+		if (!visited1[it]) {
+			dfs1(it);
+		}
+	}
+	return;
+}
+
+void dfs2(int node) {
+	visited2[node] = 1;
+	for (auto it : graph2[node]) {
+		if (!visited2[it]) {
+			dfs2(it);
+		}
+	}
+	return;
+}
 
 int main() {
 	int n, m;
 	scanf("%d%d",&n,&m);
 	int a, b;
-	for (int i = 1; i <= n; ++i) {
-		disjoint[i] = i;
-	}
-	cnt = n;
 	for (int i = 0; i < m; ++i) {
 		scanf("%d%d",&a,&b);
-		int pa = a, pb = b;
-		for (;pa != disjoint[pa];) {
-			pa = disjoint[pa];
-		}
-		for (;pb != disjoint[pb];) {
-			pb = disjoint[pb];
-		}
-		if (pa == pb)continue;
-		if (height[pb] > height[pa])swap(pa,pb);
-		disjoint[pb] = pa;
-		disjoint[b] = pa;
-		if (height[pa] == height[pb])++height[pa];
-		--cnt;
+		graph1[a].push_back(b);
+		graph2[b].push_back(a);
 	}
-	if (cnt > 1) {
-		// for (int i = 1; i <= n; ++i) {
-		// 	printf("%d ",disjoint[i]);
-		// }
-		// printf("\n");
-		for (int i = 1; i <= n; ++i) {
-			int p = i;
-			int idx = vctr.size();
-			for (;p != disjoint[p];) {
-				vctr.push_back(p);
-				p = disjoint[p];
-			}
-			for (int j = idx; j < vctr.size(); ++j) {
-				disjoint[vctr[j]] = p;
-			}
+	dfs1(1);
+	for (int i = 1; i <= n; ++i) {
+		if (!visited1[i]) {
+			printf("NO\n%d %d",1,i);
+			return 0;		
 		}
-		// for (int i = 1; i <= n; ++i) {
-		// 	printf("%d ",disjoint[i]);
-		// }
-		// printf("\n");
-		for (int i = 2; i <= n; ++i) {
-			if (disjoint[1] != disjoint[i]) {
-				printf("NO\n%d %d",1,i);
-				return 0;
-			}
+	}
+	dfs2(1);
+	for (int i = 1; i <= n; ++i) {
+		if (!visited2[i]) {
+			printf("NO\n%d %d",i,1);
+			return 0;		
 		}
 	}
 	printf("YES");
-
-
-
 	return 0;
 }
