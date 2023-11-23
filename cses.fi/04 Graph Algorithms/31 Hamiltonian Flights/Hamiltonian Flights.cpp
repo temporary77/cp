c32c5cde0bf32206e51f5c0fa287d6f826c18e55
temporary77 <thanabodhi77.n@gmail.com>
2023-11-23 13:23:16 +0800

    push

--- cses.fi/04 Graph Algorithms/31 Hamiltonian Flights/Hamiltonian Flights.cpp
+++ cses.fi/04 Graph Algorithms/31 Hamiltonian Flights/Hamiltonian Flights.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 vector<int> graph[21];
 int ans[21][1050000];
-bool visited[21][1050000];
 const int mod = 1e9+7;
 int n, m;
-
+ 
 int main() {
 	scanf("%d%d",&n,&m);
 	int a, b;
@@ -18,7 +17,10 @@ int main() {
 	}
 	ans[0][1] = 1;
 	for (int i = 1; i < (1 << n); ++i) {
+		if (!(i&1))continue;
+		if (i&(1 << n-1) && i != (1 << n)-1)continue;
 		for (int j = 0; j < n; ++j) {
+			if (!(i&(1 << j)))continue;
 			for (auto it : graph[j]) {
 				if (!(i&(1 << it))) {
 					ans[it][i^(1 << it)] += ans[j][i];
