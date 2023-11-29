937559ea519cdbc29a830408cc828ad3e8040110
temporary77 <thanabodhi77.n@gmail.com>
2023-11-29 08:23:30 +0800

    push

--- forces/c2ladders/1500/023 1499C Minimum Grid Path/1499C.cpp
+++ forces/c2ladders/1500/023 1499C Minimum Grid Path/1499C.cpp
@@ -8,15 +8,17 @@ int main() {
 		int n;
 		scanf("%d",&n);
 		long long cur[2] = {0,0};
+		int min1[2] = {INT_MAX,INT_MAX};
 		long long cost[2] = {(long long)1e16,(long long)1e16};
 		long long ans = LLONG_MAX;
 		for (int i = 0; i < n; ++i) {
 			int c;
 			scanf("%d",&c);
 			int idx = i&1;
-			cost[idx] = cur[idx]+(n-i/2)*(long long)c;
-			ans = min(ans,cost[0]+cost[1]);
+			min1[idx] = min(min1[idx],c);
 			cur[idx] += c;
+			cost[idx] = cur[idx]-min1[idx]+(n-i/2)*(long long)min1[idx];
+			ans = min(ans,cost[0]+cost[1]);
 		}
 		printf("%lld\n",ans);
 	}
