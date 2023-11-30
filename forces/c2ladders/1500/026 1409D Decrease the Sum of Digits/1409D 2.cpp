1ed4c2e43baa8cb4ea6423a6344a9aa9f5a73d8f
temporary77 <thanabodhi77.n@gmail.com>
2023-11-30 23:13:02 +0700

    push

--- forces/c2ladders/1500/026 1409D Decrease the Sum of Digits/1409D 2.cpp
+++ forces/c2ladders/1500/026 1409D Decrease the Sum of Digits/1409D 2.cpp
@@ -12,25 +12,18 @@ int main() {
 		scanf("%lld%d",&n,&s);
 		int r;
 		long long ni = n;
+		int ttl = 0;
 		for (int i = 0; i < 20; ++i) {
 			arr[i] = ni%10;
 			ni /= 10;
+			ttl += arr[i];
 			if (ni == 0) {
 				r = i;
 				arr[i+1] = 0;
 				break;
 			}
 		}
-		int ttl = 0;
-		int idx = r;
-		for (;idx >= 0;) {
-			ttl += arr[idx];
-			if (ttl > s)break;
-			--idx;
-		}
-		for (int i = idx-1; i >= 0; --i) {
-			arr[i] = 0;
-		}
+		int idx = 0;
 		for (;ttl > s;) {
 			ttl += 10-arr[idx];
 			arr[idx] = 10;
@@ -43,9 +36,9 @@ int main() {
 			}
 		}
 		long long trgt = 0;
-		for (int j = max(r,idx); j >= 0; --j) {
+		for (int i = max(r,idx); i >= 0; --i) {
 			trgt *= 10;
-			trgt += arr[j];
+			trgt += arr[i];
 		}
 		printf("%lld\n",trgt-n);
 	}
