f5879e7cece9f4066c6f52be7d3aa6419a8b6121
temporary77 <thanabodhi77.n@gmail.com>
2023-12-01 20:25:08 +0800

    push

--- forces/c2ladders/1500/027 1399D Binary String To Subsequence/1399D.cpp
+++ forces/c2ladders/1500/027 1399D Binary String To Subsequence/1399D.cpp
@@ -1,11 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+stack<int> stck[2];
+queue<int> que;
+
 int main() {
-
-
-
-
-
+	cin.tie(NULL);
+	int t;
+	scanf("%d",&t);
+	string str;
+	for (int ti = 0; ti < t; ++ti) {
+		int n;
+		scanf("%d",&n);
+		cin >> str;
+		int idx = 0;
+		for (auto it : str) {
+			int x = it-'0';
+			if (stck[x^1].empty()) {
+				++idx;
+				stck[x].push(idx);
+				que.push(idx);
+			} else {
+				stck[x].push(stck[x^1].top());
+				stck[x^1].pop();
+				que.push(stck[x].top());
+			}
+		}
+		printf("%d\n",stck[0].size()+stck[1].size());
+		for (;!que.empty();) {
+			printf("%d ",que.front());
+			que.pop();
+		}
+		printf("\n");
+		for (;!stck[0].empty();)stck[0].pop();
+		for (;!stck[1].empty();)stck[1].pop();
+	}
 	return 0;
 }
