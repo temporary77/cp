a8ba0883b38e1dc254a5942bc238bb084456afbb
temporary77 <thanabodhi77.n@gmail.com>
2023-12-12 18:07:46 -0900

    push

--- forces/c2ladders/1600/005 1632C Strange Test/1632C.cpp
+++ forces/c2ladders/1600/005 1632C Strange Test/1632C.cpp
@@ -11,7 +11,37 @@ int main() {
 			printf("%d\n",a-b);
 			continue;
 		}
-		
+		int ans = 0;
+		for (int i = 0; i < 20; ++i) {
+			// printf("%d %d %d\n",a,b,i);
+			if (a&(1 << i) && !(b&(1 << i))) {
+				printf("%d %d %d\n",i,a,b);
+				for (int j = i+1; j < 20; ++j) {
+					if (!(a&(1 << j)) && b&(1 << j)) {
+						ans += (1 << i);
+						a += (1 << i);
+						break;
+					}
+				}
+			}
+		}
+		if (a >= b) {
+			ans += a-b;
+			printf("%d\n",ans);
+			continue;	
+		}
+		++ans;
+		a |= b;
+		ans += a-b;
+		printf("%d\n",ans);
 	}
 	return 0;
 }
+
+/*
+1
+3
+2
+1
+23329
+*/
